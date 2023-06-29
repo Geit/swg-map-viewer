f77d6e04216573dc447025151d1e9eacf676b493
Rory King <rory_king@hotmail.co.uk>
2023-06-29 20:48:41 +0100

    Set build folder for vite to what the deployer expects

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -6,4 +8,7 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   plugins: [react(), svgr()],
   root: 'src',
+  build: {
+    outDir: path.join(__dirname, 'build'),
+  },
 });
